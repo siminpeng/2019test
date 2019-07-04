777539ca73822293a980e8dbf5ad5e5372f6d772
pengsimin <pengsimin429@pingan.com.cn>
2019-07-04 16:03:54 +0800

    再一次测试

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yingying
+ * Date: 2019/7/4
+ * Time: 4:01 PM
+ */
+
+echo "hello world";
---
+++ test01/tools.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yingying
+ * Date: 2019/7/4
+ * Time: 4:02 PM
+ */
+echo 'this is a tool test php';
